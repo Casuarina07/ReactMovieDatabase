a4721190901716c24658da29ab6efcf6604c2def
Casuarina07 <31846397+Casuarina07@users.noreply.github.com>
2020-06-17 17:32:51 +0800

    https

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         selected: {}
     });
 
-    const apiurl = "https://www.omdbapi.com/?apikey=5493cb7e";
+    const apiurl = "https://omdbapi.com/?apikey=5493cb7e";
 
     const search = (e) => {
         if (e.key === "Enter") {
